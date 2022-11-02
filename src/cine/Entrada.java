2df09bbeaf9f7104b3a01bb848b45c2a98d4032a
Gonzalo Peña Bravo <gonzalo.pena@opendeusto.es>
2022-11-02 17:43:40 +0100

    Actualización clases Espectador y Entrada

--- src/cine/Entrada.java
+++ src/cine/Entrada.java
@@ -1,5 +1,134 @@
 package cine;
 
+import java.sql.Date;
+import java.util.Objects;
+import java.util.UUID;
+
 public class Entrada {
 
+		UUID id;
+		protected Date fecha;
+		protected int butaca;
+		protected int precio;
+		protected Pelicula pelicula;
+		protected Complementos complemento;
+		protected Espectador espectador;
+		protected Sala sala;
+		
+		public Entrada(UUID id, Date fecha, int butaca, int precio, Pelicula pelicula, Complementos complemento,
+				Espectador espectador, Sala sala) {
+			super();
+			
+			this.id = UUID.randomUUID ();
+			this.fecha = fecha;
+			this.butaca = butaca;
+			this.precio = precio;
+			this.setPelicula(pelicula);;
+			this.setComplemento(complemento);
+			this.setEspectador(espectador);
+			this.setSala(sala);
+		}
+		
+		public Entrada() {
+			super();
+			
+			this.id = UUID.randomUUID ();
+			this.fecha = fecha;
+			this.butaca = 0;
+			this.precio = 0;
+		}
+
+		public UUID getId() {
+			return id;
+		}
+
+		public void setId(UUID id) {
+			this.id = id;
+		}
+
+		public Date getFecha() {
+			return fecha;
+		}
+
+		public void setFecha(Date fecha) {
+			this.fecha = fecha;
+		}
+
+		public int getButaca() {
+			return butaca;
+		}
+
+		public void setButaca(int butaca) {
+			this.butaca = butaca;
+		}
+
+		public int getPrecio() {
+			return precio;
+		}
+
+		public void setPrecio(int precio) {
+			this.precio = precio;
+		}
+
+		public Pelicula getPelicula() {
+			return pelicula;
+		}
+
+		public void setPelicula(Pelicula pelicula) {
+			this.pelicula = pelicula;
+		}
+
+		public Complementos getComplemento() {
+			return complemento;
+		}
+
+		public void setComplemento(Complementos complemento) {
+			this.complemento = complemento;
+		}
+
+		public Espectador getEspectador() {
+			return espectador;
+		}
+
+		public void setEspectador(Espectador espectador) {
+			this.espectador = espectador;
+		}
+
+		public Sala getSala() {
+			return sala;
+		}
+
+		public void setSala(Sala sala) {
+			this.sala = sala;
+		}
+
+		@Override
+		public String toString() {
+			return "Entrada [id=" + id + ", fecha=" + fecha + ", butaca=" + butaca + ", precio=" + precio
+					+ ", pelicula=" + pelicula + ", complemento=" + complemento + ", espectador=" + espectador
+					+ ", sala=" + sala + "]";
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(id);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Entrada other = (Entrada) obj;
+			return Objects.equals(id, other.id);
+		}
+		
+		
+		
+		
+		
+		
 }
--- src/cine/Espectador.java
+++ src/cine/Espectador.java
@@ -1,5 +1,59 @@
 package cine;
 
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.UUID;
+
 public class Espectador {
 
+		UUID id;
+		protected ArrayList<Entrada> entradas;
+		
+		public Espectador(UUID id, ArrayList<Entrada> entradas) {
+			super();
+			this.id = UUID.randomUUID ();
+			this.setEntradas(new ArrayList<Entrada>(entradas));
+		}
+
+		public UUID getId() {
+			return id;
+		}
+
+		public void setId(UUID id) {
+			this.id = id;
+		}
+
+		public ArrayList<Entrada> getEntradas() {
+			return entradas;
+		}
+
+		public void setEntradas(ArrayList<Entrada> entradas) {
+			this.entradas = entradas;
+		}
+
+		@Override
+		public String toString() {
+			return "Espectador [id=" + id + ", entradas=" + entradas + "]";
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(entradas, id);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Espectador other = (Espectador) obj;
+			return Objects.equals(entradas, other.entradas) && Objects.equals(id, other.id);
+		}
+		
+		
+		
+		
 }
