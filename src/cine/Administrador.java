ff880864b028d4c4d04a355b931359a98a3a4f88
Francisco Martín Perea <fran.martin@opendeusto.es>
2022-11-02 11:27:35 +0100

    Actualización de la clase Administrador

--- src/cine/Administrador.java
+++ src/cine/Administrador.java
@@ -1,5 +1,38 @@
 package cine;
 
-public class Administrador {
+import java.util.ArrayList;
+import java.util.UUID;
 
+public class Administrador extends Usuario{
+	
+	protected String llave;
+	protected ArrayList<SetPeliculas> adminDpeliculas;
+	
+	
+	public Administrador(String nombre, String contraseña, UUID id, String llave,
+			ArrayList<SetPeliculas> adminDpeliculas) {
+		super(nombre, contraseña, id);
+		this.llave = llave;
+		this.adminDpeliculas = adminDpeliculas;
+	}
+	
+	public String getLlave() {
+		return llave;
+	}
+	public void setLlave(String llave) {
+		this.llave = llave;
+	}
+
+	public ArrayList<SetPeliculas> getAdminDpeliculas() {
+		return adminDpeliculas;
+	}
+	public void setAdminDpeliculas(ArrayList<SetPeliculas> adminDpeliculas) {
+		this.adminDpeliculas = adminDpeliculas;
+	}
+
+	@Override
+	public String toString() {
+		return "Administrador [llave=" + llave + ", adminDpeliculas=" + adminDpeliculas + "]";
+	}
+	
 }
