bba476bc9277d148f51b3f23bc22d268b7204758
arrieniker <iker.arrien@opendeusto.es>
2022-11-02 22:12:22 +0100

    Gestor de Bases de datos con sus debidas comprobaciones en el main

--- src/cine/GestorBD.java
+++ src/cine/GestorBD.java
@@ -8,13 +8,13 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.time.Duration;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class GestorBD {
-    protected static final String DRIVER_NAME = "org.sqlite.JDBC";
-    protected static final String DATABASE_FILE = "db/database.db";
-    protected static final String CONNECTION_STRING = "jdbc:sqlite:" + DATABASE_FILE;
+
+	protected static final String DRIVER_NAME = "org.sqlite.JDBC";
+	protected static final String DATABASE_FILE = "db/database.db";
+	protected static final String CONNECTION_STRING = "jdbc:sqlite:" + DATABASE_FILE;
 	
 	public GestorBD() {		
 		try {
@@ -33,8 +33,8 @@ public class GestorBD {
 		     Statement stmt = con.createStatement()) {
 			
 	        String sql = "CREATE TABLE IF NOT EXISTS PELICULA (\n"
-	                   + " ID_PELICULA STRING PRIMARY KEY,\n"
-	                   + " NOMBRE TEXT NOT NULL,\n"
+	                   + " ID_PELICULA STRING ,\n"
+	                   + " NOMBRE TEXT PRIMARY KEY NOT NULL,\n"
 	                   + " VALORACION DOUBLE NOT NULL,\n"
 	                   + " DIRECTOR TEXT NOT NULL,\n"
 	                   + " DURACION INTEGER NOT NULL,\n"
@@ -43,7 +43,7 @@ public class GestorBD {
 	                   + ");";
 	        	        
 	        if (!stmt.execute(sql)) {
-	        	System.out.println("- Se ha creado la tabla Pelicula");
+	        	System.out.println("- Se ha creado la tabla pelicula");
 	        }
 		} catch (Exception ex) {
 			System.err.println(String.format("* Error al crear la BBDD: %s", ex.getMessage()));
@@ -52,15 +52,15 @@ public class GestorBD {
 	}
 	
 	public void borrarBBDD() {
-		//Se abre la conexion y se obtiene el Statement
+		//Se abre la conexión y se obtiene el Statement
 		try (Connection con = DriverManager.getConnection(CONNECTION_STRING);
 		     Statement stmt = con.createStatement()) {
 			
 	        String sql = "DROP TABLE IF EXISTS PELICULA";
 			
-	        //Se ejecuta la sentencia de creacion de la tabla Estudiantes
+	        //Se ejecuta la sentencia de creación de la tabla Estudiantes
 	        if (!stmt.execute(sql)) {
-	        	System.out.println("- Se ha borrado la tabla Pelicula");
+	        	System.out.println("- Se ha borrado la tabla pelicula");
 	        }
 		} catch (Exception ex) {
 			System.err.println(String.format("* Error al borrar la BBDD: %s", ex.getMessage()));
@@ -82,13 +82,13 @@ public class GestorBD {
 		try (Connection con = DriverManager.getConnection(CONNECTION_STRING);
 		     Statement stmt = con.createStatement()) {
 			//Se define la plantilla de la sentencia SQL
-			String sql = "INSERT INTO PELICULA (ID_PELICULA ,NOMBRE, VALORACION, DIRECTOR, DURACION, EDAD_RECOMENDADA, GENEROS) VALUES ('%s','%s', '%f', '%s', '%s', '%s', '%s');";
+			String sql = "INSERT INTO PELICULA (ID_PELICULA, NOMBRE, VALORACION, DIRECTOR, DURACION, EDAD_RECOMENDADA, GENEROS) VALUES ('%s', '%s', '%f','%s', '%d', '%s', '%s');";
 			
 			System.out.println("- Insertando peliculas...");
 			
 			//Se recorren los clientes y se insertan uno a uno
 			for (Pelicula p : peliculas) {
-				if (1 == stmt.executeUpdate(String.format(sql, p.getId().toString(), p.getNombre(), p.getValoracion(), p.getDirector(), p.getDuracion(), p.getEdad(),p.getGeneros()))) {					
+				if (1 == stmt.executeUpdate(String.format(sql, p.getId().toString(), p.getNombre(), p.getValoracion(), p.getDirector(),(int) p.getDuracion().toMinutes(), p.getEdad(), Genero.ACCION))) {					
 					System.out.println(String.format(" - Pelicula insertada: %s", p.toString()));
 				} else {
 					System.out.println(String.format(" - No se ha insertado la pelicula: %s", p.toString()));
@@ -114,23 +114,23 @@ public class GestorBD {
 			
 			//Se recorre el ResultSet y se crean objetos Cliente
 			while (rs.next()) {
-				pelicula = new Pelicula();
+				pelicula = new Pelicula ();
+
+                pelicula.setNombre (rs.getString ("NOMBRE"));
+                pelicula.setValoracion (rs.getDouble ("VALORACION"));
+                pelicula.setDirector (rs.getString ("DIRECTOR"));
+                pelicula.setDuracion (Duration.ofMinutes (rs.getInt ("DURACION")));
+                pelicula.setEdad (EdadRecomendada.valueOf (rs.getString ("EDAD_RECOMENDADA")));
+                pelicula.setGeneros (new ArrayList<> ());
+                
+                peliculas.add(pelicula);
 				
-				pelicula.setNombre(rs.getString("NOMBRE"));
-				pelicula.setValoracion(rs.getDouble("VALORACION"));
-				pelicula.setDirector(rs.getString("DIRECTOR"));
-				pelicula.setDuracion(Duration.ofMinutes(rs.getInt("DURACION")));
-				pelicula.setEdad(EdadRecomendada.valueOf(rs.getString("EDAD_RECOMENDADA")));
-				pelicula.setGeneros(new ArrayList<>());
-				
-				//Se inserta cada nueva pelicula en la lista de peliculas
-				peliculas.add(pelicula);
 			}
 			
 			//Se cierra el ResultSet
 			rs.close();
 			
-			System.out.println(String.format("- Se han recuperado %d pelicula...", peliculas.size()));			
+			System.out.println(String.format("- Se han recuperado %d clientes...", peliculas.size()));			
 		} catch (Exception ex) {
 			System.err.println(String.format("* Error al obtener datos de la BBDD: %s", ex.getMessage()));
 			ex.printStackTrace();						
@@ -144,9 +144,9 @@ public class GestorBD {
 		try (Connection con = DriverManager.getConnection(CONNECTION_STRING);
 		     Statement stmt = con.createStatement()) {
 			//Se ejecuta la sentencia de borrado de datos
-			String sql = "UPDATE PELICULA SET VALORACION = '%s' WHERE ID_PELICULA = '%s';";
+			String sql = "UPDATE PELICULA SET VALORACION = '%f' WHERE NOMBRE = '%s' ;";
 			
-			int result = stmt.executeUpdate(String.format(sql, newValoracion, pelicula.getId()));
+			int result = stmt.executeUpdate(String.format(sql, newValoracion, pelicula.getNombre()));
 			
 			System.out.println(String.format("- Se ha actulizado %d peliculas", result));
 		} catch (Exception ex) {
@@ -163,10 +163,10 @@ public class GestorBD {
 			String sql = "DELETE FROM PELICULA;";			
 			int result = stmt.executeUpdate(sql);
 			
-			System.out.println(String.format("- Se han borrado %d peliculas", result));
+			System.out.println(String.format("- Se han borrado %d pelicula", result));
 		} catch (Exception ex) {
 			System.err.println(String.format("* Error al borrar datos de la BBDD: %s", ex.getMessage()));
 			ex.printStackTrace();						
 		}		
-	}
+	}	
 }
